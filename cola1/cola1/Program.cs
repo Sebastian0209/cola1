0f8cf04bfd07a915a89663028b362b3ee2887696
SCRV <SCRV@DESKTOP-PM5MI5A>
2020-03-10 21:16:52 -0400

    FALTA LA CONEXION

---
+++ cola1/cola1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using System;
+
+namespace cola1
+{
+
+    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string ServiceBusConnectionString = "<Endpoint=sb://csrsi410.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AUVeZb+wLwaVqKNImMwqo+2lP5I8gYrIB6OiH8QrjrY=>";
+            const string QueueName = "<qprueba>";
+            IQueueClient queueClient;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            string messageBody = $"Message prueba de computacion movil";
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            // Write the body of the message to the console.
+            //Console.WriteLine($"Sending message: {messageBody}");
+
+            // Send the message to the queue.
+            await queueClient.SendAsync(message);
+
+//Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ cola1/solucion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+namespace Consumidor
+{
+    class Program
+    {
+        static IQueueClient queueClient;
+        static async Task Main(string[] args)
+        {
+            string ServiceBusConnectionString = "Endpoint=sb://serviciobus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=l/xeKXQ3iPYHJbFxCB6FOO3LWmgvr/SMyC8l2gUy38k=";
+            string QueueName = "qprueba";
+
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+            Console.ReadKey();
+            await queueClient.CloseAsync();
+
+
+        }
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
